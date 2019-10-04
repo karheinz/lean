f515f826e1f13b2f63b21a5771faee2ca217f5be
Karsten Heinze <karsten@sidenotes.de>
2019-10-04 22:40:13 +0200

    Add serialization of tasks to YAML
    Uses serde, serde_yaml and yaml_rust.

--- src/core.rs
+++ src/core.rs
@@ -1,17 +1,30 @@
+extern crate serde;
+
 use std::any::Any;
+use serde::{Serialize, Deserialize};
+use serde::ser::Serializer;
 
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Serialize)]
 pub enum Occurrence {
     Daily,
+    /// weekday
     Weekly(u32),
+    /// week, weekday
     Monthly(u32, u32),
+}
+
+#[derive(Debug,PartialEq,Serialize)]
+pub enum Duration {
+    Minutes(u32),
+    Hours(f64),
 }
 
 pub trait TaskParams {
     fn as_any(&self) -> &dyn Any;
 }
 
+#[derive(Serialize)]
 pub struct OneTime {
 }
 
@@ -21,8 +34,10 @@ impl TaskParams for OneTime {
     }
 }
 
+#[derive(Serialize)]
 pub struct Periodic {
     pub occurrence: Occurrence,
+    pub duration: Duration,
 }
 
 impl TaskParams for Periodic {
@@ -40,25 +55,32 @@ impl TaskParams for Periodic {
 /// Specific params can be retrieved like this:
 ///
 /// ```
-/// use lean::core::{Task, Periodic, Occurrence};
+/// use lean::core::{Task, Periodic, Occurrence, Duration};
 ///
 /// let mut task = Task {
 ///     title: String::from("Title"),
 ///     description: String::from("Description"),
 ///     effort: vec![],
-///     params: Box::new(Periodic { occurrence: Occurrence::Weekly(1) }),
+///     params: Box::new(Periodic { occurrence: Occurrence::Weekly(1),
+///         duration: Duration::Hours(1.0) }),
 ///     };
 ///
 /// match task.params::<Periodic>() {
-///     Some(params) => assert_eq!(Occurrence::Weekly(1), params.occurrence),
+///     Some(params) => {
+///         assert_eq!(Occurrence::Weekly(1), params.occurrence);
+///         assert_eq!(Duration::Hours(1.0), params.duration);
+///     },
 ///     _ => {}
 /// };
 /// ```
+#[derive(Serialize, Deserialize)]
 pub struct Task {
     pub title: String,
     pub description: String,
     //tags: Vec<Tag>,
     pub effort: Vec<f64>,
+    #[serde(serialize_with="Task::params_serialize", skip_deserializing)]
+    #[serde(default="Task::get_default_params")]
     pub params: Box<dyn TaskParams>,
     //created_at: Time,
     //required_at: Option<Time>,
@@ -73,6 +95,10 @@ pub struct Task {
 }
 
 impl Task {
+    fn get_default_params() -> Box<dyn TaskParams> {
+        Box::new(OneTime {})
+    }
+
     pub fn is_valid(&self) -> bool {
         ! self.is_invalid()
     }
@@ -86,6 +112,18 @@ impl Task {
         where T: 'static {
         self.params.as_any().downcast_ref::<T>()
     }
+
+    fn params_serialize<S>(params: &Box<dyn TaskParams>, s: S)
+        -> Result<S::Ok, S::Error>
+        where S: Serializer {
+        if let Some(one_time) = params.as_any().downcast_ref::<OneTime>() {
+            one_time.serialize(s)
+        } else if let Some(periodic) = params.as_any().downcast_ref::<Periodic>() {
+            periodic.serialize(s)
+        } else {
+            panic!("unknown TaskParams type")
+        }
+    }
 }
 
 #[cfg(test)]
@@ -98,7 +136,8 @@ mod tests {
             title: String::from("Title"),
             description: String::from("Description"),
             effort: vec![],
-            params: Box::new(Periodic { occurrence: Occurrence::Weekly(1) }),
+            params: Box::new(Periodic { occurrence: Occurrence::Weekly(1),
+                duration: Duration::Hours(1.0) }),
         };
 
         assert_eq!("Title", task.title);
@@ -127,6 +166,31 @@ mod tests {
         if let Some(_) = task.params::<Periodic>() {
             return Err(format!("got Periodic params instead of OneTime"));
         }
+
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_a_task() -> Result<(), String> {
+        let mut task = Task {
+            title: String::from("Title"),
+            description: String::from("Description"),
+            effort: vec![],
+            //params: Box::new(Periodic { occurrence: Occurrence::Weekly(1),
+            params: Box::new(Periodic { occurrence: Occurrence::Daily,
+                duration: Duration::Hours(1.0) }),
+        };
+
+        match serde_yaml::to_string(&task) {
+            Ok(y) => println!("{}", y),
+            Err(reason) => return Err(format!("{}", reason)),
+        };
+
+        task.params = Box::new(OneTime {});
+        match serde_yaml::to_string(&task) {
+            Ok(y) => println!("{}", y),
+            Err(reason) => return Err(format!("{}", reason)),
+        };
 
         Ok(())
     }
