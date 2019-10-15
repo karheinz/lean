055f5e2499b6cff276312feb80381206be392556
Karsten Heinze <karsten@sidenotes.de>
2019-10-15 20:27:07 +0200

    Introduce sub commands

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,33 @@ use lean::cli::*;
 use std::env;
 
 
-fn parse(args: &[String]) -> Result<Box<dyn Command>, String> {
-    let arg1 = args.get(1);
-
-    let mut command_str: Option<&str> = None;
-    if let Some(arg1_value) = arg1 {
-        command_str = Some(&arg1_value[..]);
+fn get_cmd(args: &[String]) -> Option<&str> {
+    if let Some(value) = args.get(0) {
+        Some(&value[..])
+    } else {
+        None
     }
+}
 
-    match command_str {
+fn parse(args: &[String]) -> Result<Box<dyn Command>, String> {
+    match get_cmd(&args[1..]) {
         Some("help") => Ok(Box::new(ShowHelp::new(&args[0], &args[2..])?)),
         Some("init") => Ok(Box::new(InitWorkspace::new(&args[2..])?)),
-        Some("add") => Ok(Box::new(AddTask::new(&args[2..])?)),
-        Some("list") => Ok(Box::new(ListTasks::new(&args[2..])?)),
-        Some("show") => Ok(Box::new(ShowTasks::new(&args[2..])?)),
+        Some("tasks") => {
+            match get_cmd(&args[2..]) {
+                Some("add") => Ok(Box::new(AddTask::new(&args[3..])?)),
+                Some("list") => Ok(Box::new(ListTasks::new(&args[3..])?)),
+                Some("show") => Ok(Box::new(ShowTasks::new(&args[3..])?)),
+                Some(unknown) => Err(format!("unknown sub command {}", unknown)),
+                _ => Err(format!("missing command")),
+            }
+        },
+        Some("people") => {
+            match get_cmd(&args[2..]) {
+                Some(unknown) => Err(format!("unknown sub command {}", unknown)),
+                _ => Err(format!("missing command")),
+            }
+        },
         Some(unknown) => Err(format!("unknown command {}", unknown)),
         _ => Ok(Box::new(ShowUsage::new(&args[0])?)),
     }
